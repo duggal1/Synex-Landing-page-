cf02452d4a81eaba4af3143a4331df99ee037ae3
harshit duggal <harshitduggal56@gmail.com>
2025-03-07 23:02:12 +0530

    updated

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,21 +1,16 @@
+
 import { Metadata } from "next";
 
 export const generateMetadata = ({
-    title = `${process.env.NEXT_PUBLIC_APP_NAME} | Home`,
-    description = `Manage your real estate properties with ease.`,
-    image = "/thumbnail.png",
-    icons = [
-        {
-            rel: "apple-touch-icon",
-            sizes: "32x32",
-            url: "/icons/icon.svg"
-        },
-        {
-            rel: "icon",
-            sizes: "32x32",
-            url: "/icons/icon.svg"
-        },
-    ],
+    title = 'SynexAI - AI Agent Invoice Automation ',
+    description = `Automate invoice processing, reduce errors, and get paid faster than ever  with Synex AI.`,
+    image = "/icons/synex-thumbnail.png",
+    icons = {
+        icon: [
+            { rel: "icon", sizes: "32x32", url: "/icons/synex-thumbnail.png", type: "image/png" },
+            { rel: "apple-touch-icon", sizes: "180x180", url: "/icons/synex-thumbnail.png" }
+        ]
+    },
     noIndex = false
 }: {
     title?: string;
@@ -26,6 +21,18 @@ export const generateMetadata = ({
 } = {}): Metadata => ({
     title,
     description,
+    openGraph: {
+        title,
+        description,
+        images: [{ url: image ?? "/icons/synex-thumbnail.png", width: 1200, height: 630, alt: "Synex AI Preview" }],
+        type: "website"
+    },
+    twitter: {
+        card: "summary_large_image",
+        title,
+        description,
+        images: [image ?? "/icons/synex-thumbnail.png"]
+    },
     icons,
-    ...(noIndex && { robots: { index: false, follow: false } }),
+    ...(noIndex && { robots: { index: false, follow: false } })
 });
